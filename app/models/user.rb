5cb7e7ea44049a6bb76db3b2cd185f47ddae7a62
Sho50302 <shogo.s1226@docomo.ne.jp>
2020-05-10 14:04:48 +0900

    user モデル 編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname,           presence: true, length: {maximum: 30}
+  validates :email,              presence: true, uniqueness: true
+  validates :encrypted_password, presence: :true
+
+  has_many :tasks
+  has_many :groups, through: :groups_users
+  has_many :groups_users
 end
+# , format: {with: /\A[a-zA-Z0-9_#!$%&`'*+-{|}~^\/=?.]+@[a-zA-Z0-9][a-zA-Z0-9.-]+\z/}
+# , length: {minimum: 7}
