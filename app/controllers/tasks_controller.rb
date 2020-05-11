1c2bcffe96102d2ad53630bd7c30e8e12a3a52f5
Sho50302 <shogo.s1226@docomo.ne.jp>
2020-05-11 16:22:21 +0900

    ビュー 編集

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,18 +2,46 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    task = Task.find(params[:id])
+    task.update(task_params)
+    redirect_to tasks_path
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :content, :start_date, :start_time_hour, :start_time_minute, :end_date, :end_time_hour, :end_time_minute).merge(user_id: current_user.id)
   end
   
 end
