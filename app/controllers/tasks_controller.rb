a306a06d0ea83942379bee1dcfbc4c381b5cbf26
Sho50302 <shogo.s1226@docomo.ne.jp>
2020-05-15 14:37:07 +0900

    コントローラー 編集

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
+  before_action :set_group, except: [:index, :user_new, :user_create, :user_edit, :user_update, :user_show, :user_destroy]
 
   def index
-    @tasks = Task.all
+    @tasks = Task.includes(:user)
   end
 
   def new
@@ -11,7 +12,7 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
     if @task.save
-      redirect_to tasks_path
+      redirect_to group_path(@group)
     else
       render :new
     end
@@ -24,23 +25,65 @@ class TasksController < ApplicationController
   def update
     task = Task.find(params[:id])
     task.update(task_params)
-    redirect_to tasks_path
+    redirect_to group_path(@group)
   end
 
   def show
-    @task = Task.find(params[:id])
+    @task = @group.tasks.find(params[:id])
   end
 
   def destroy
     task = Task.find(params[:id])
     task.destroy
-    redirect_to tasks_path
+    redirect_to group_path(@group)
+  end
+
+  def user_new
+    @task = Task.new
+  end
+
+  def user_create
+    @task = Task.new(user_task_params)
+    if @task.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def user_edit
+    @task = Task.find(params[:id])
+  end
+
+  def user_update
+    task = Task.find(params[:id])
+    task.update(user_task_params)
+    redirect_to root_path
+  end
+
+  def user_show
+    @task = Task.find(params[:id])
+  end
+
+  def user_destroy
+    task = Task.find(params[:id])
+    task.destroy
+    redirect_to root_path
   end
 
   private
 
   def task_params
+    params.require(:task).permit(:title, :content, :start_date, :start_time_hour, :start_time_minute, :end_date, :end_time_hour, :end_time_minute).merge(user_id: current_user.id, group_id: @group.id)
+  end
+
+  def user_task_params
     params.require(:task).permit(:title, :content, :start_date, :start_time_hour, :start_time_minute, :end_date, :end_time_hour, :end_time_minute).merge(user_id: current_user.id)
+  end
+
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
   
 end
