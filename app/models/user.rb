6f4d2507130b7a179b1fa834b7cb8583c942d494
Sho50302 <shogo.s1226@docomo.ne.jp>
2020-05-17 18:30:44 +0900

    user バリデーション  設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,10 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :nickname,           presence: true, length: {maximum: 30}
-  validates :email,              presence: true, uniqueness: true
-  validates :encrypted_password, presence: :true
+  validates :nickname,           presence: true, length: {maximum: 20}
+  validates :email,              presence: true, uniqueness: true, format: {with: /\A[a-zA-Z0-9_#!$%&`'*+-{|}~^\/=?.]+@[a-zA-Z0-9][a-zA-Z0-9.-]+\z/}
+  validates :encrypted_password, presence: :true, length: {minimum: 7}, format: {with: /\A(?=.*?[a-z])(?=.*?\d)(?=.*?[!-\/:-@\[-`{-~])[!-~]{7,100}+\z/i}
+  
 
   has_many :group_users
   has_many :groups, through: :group_users
@@ -15,5 +16,5 @@ class User < ApplicationRecord
   mount_uploader :user_image, UserImageUploader
 
 end
-# , format: {with: /\A[a-zA-Z0-9_#!$%&`'*+-{|}~^\/=?.]+@[a-zA-Z0-9][a-zA-Z0-9.-]+\z/}
-# , length: {minimum: 7}
+
+
